51acb55a08c31fff81e469f76e3cb1e8fb0c24e1
Your Name <you@example.com>
2024-04-15 18:04:03 -0400

    umm

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -1,33 +1,37 @@
+import pygame
 
 class Controller:
   
   def __init__(self):
-    #setup pygame data
-    
+    pygame.init()
+    self.screen = pygame.display.set_mode((800, 600))
+    self.clock = pygame.time.Clock()
+    self.running = True
+
   def mainloop(self):
-    #select state loop
-    
-  
-  ### below are some sample loop states ###
+    while self.running:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          self.running = False
+        elif event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_ESCAPE:
+            self.running = False
+      self.menuloop()
+      self.gameloop()
+      self.gameoverloop()
+      pygame.display.flip()
+      self.clock.tick(60)
 
   def menuloop(self):
-    
-      #event loop
+    pass  # Ill do this l8r
 
-      #update data
-
-      #redraw
-      
   def gameloop(self):
-      #event loop
+    pass  # Ill do this l8r
 
-      #update data
-
-      #redraw
-    
   def gameoverloop(self):
-      #event loop
+    pass  # Ill do this l8r
 
-      #update data
-
-      #redraw
+if __name__ == "__main__":
+  controller = Controller()
+  controller.mainloop()
+  pygame.quit()
